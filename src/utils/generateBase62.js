1f14586d793310ab810249579805091fbf717bdb
Sahaj-jj <sahajjain13@gmail.com>
2023-05-03 15:19:55 +0530

    feat: Add function to generate base-62 value from uuid

---
+++ src/utils/generateBase62.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto");
+
+function generateBase62() {
+    // Generate a random 64-bit ID using the crypto module
+    const id = crypto.randomBytes(6).readUIntLE(0, 6);
+
+    // Convert the ID to base 62 (using alphanumeric characters)
+    const characters =
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const base = characters.length;
+    let result = "";
+    let remaining = id;
+    do {
+        const index = remaining % base;
+        result = characters.charAt(index) + result;
+        remaining = Math.floor(remaining / base);
+    } while (remaining > 0);
+
+    // Pad the result with leading zeros to ensure it is 7 digits long
+    if (result.length > 7) {
+        return generateBase62();
+    }
+
+    return result;
+}
+
+module.exports = generateBase62;
