c28564138f2c045a1bc4be80598701f525a2a42c
Sahaj-jj <sahajjain13@gmail.com>
2023-05-03 15:18:28 +0530

    feat: Add init.sql file and its docker config

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE url_shortener_db;
+
+\c url_shortener_db;
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    short_code VARCHAR(7) UNIQUE NOT NULL,
+    original_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
