cda902457b7346a16548a11bf815893765e80316
Sahaj-jj <sahajjain13@gmail.com>
2023-05-06 18:24:09 +0530

    feat: Send user info in req object

--- server/src/middlewares/sessionChecker.js
+++ server/src/middlewares/sessionChecker.js
@@ -1,14 +1,26 @@
 const pool = require("../../db/pg");
 
+async function getUser(userId) {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [
+        userId,
+    ]);
+    const user = result.rows[0];
+    return user;
+}
+
 const sessionChecker = async (req, res, next) => {
-    if (req.session.userId) {
+    let userId = req.session.userId;
+    if (userId) {
+        req.user = await getUser(userId);
         return next();
     }
 
     const result = await pool.query(
         "INSERT INTO users DEFAULT VALUES RETURNING id"
     );
-    req.session.userId = result.rows[0].id;
+    userId = result.rows[0].id;
+    req.session.userId = userId;
+    req.user = await getUser(userId);
     return next();
 };
 
